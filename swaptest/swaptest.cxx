0adf268d3395dd8ff6966375b15cdf4bcf8b9eba
MarkSun <mark@weintek.com>
2016-05-18 18:24:46 +0800

    Modify code

--- swaptest/swaptest.cxx
+++ swaptest/swaptest.cxx
@@ -13,9 +13,6 @@
 #include <ctype.h>
 #include <errno.h>
 
-//#include <X11/Xlib.h>
-//#include <X11/keysym.h>
-
 #include <fltk/SharedImage.h>
 #include <fltk/draw.h>
 #include <fltk/run.h>
@@ -35,10 +32,6 @@
 using namespace fltk;
 
 #define KEYCODE XK_A
-//fltk::SharedImage *image ;
-//fltk::SharedImage *imagepng = fltk::pngImage::get("main.png");
-//fltk::SharedImage *imagejpeg = fltk::jpegImage::get("Ficon.jpg");
-//fltk::Thread prime_thread;
 
 SharedImage *image;
 SharedImage *imageempty;
@@ -79,30 +72,20 @@ void jpeg_cb(Widget *,void *){
 	win->redraw();
 }
 
+void textset(Widget *textdisplay,int size,const char *string){
+	textdisplay->labelsize(size); 
+	textdisplay->box(fltk::NO_BOX);
+	textdisplay->copy_label(string);  
+	textdisplay->align(fltk::ALIGN_LEFT|fltk::ALIGN_INSIDE);
+}
+
 void text_cb(Widget *,void *){
 	box->hide(); 
 	box->image((Image*)imageempty);
-	
-	textdisplay1->labelsize(32); 
-	textdisplay1->box(fltk::NO_BOX);
-	textdisplay1->copy_label("hello word");  
-	textdisplay1->align(fltk::ALIGN_LEFT|fltk::ALIGN_INSIDE);
-	
-	textdisplay2->labelsize(32); 
-	textdisplay2->box(fltk::NO_BOX);
-	textdisplay2->copy_label("hello word");  
-	textdisplay2->align(fltk::ALIGN_LEFT|fltk::ALIGN_INSIDE);
-	
-	textdisplay3->labelsize(32); 
-	textdisplay3->box(fltk::NO_BOX);
-	textdisplay3->copy_label("hello word");  
-	textdisplay3->align(fltk::ALIGN_LEFT|fltk::ALIGN_INSIDE);
-	
-	textdisplay4->labelsize(32); 
-	textdisplay4->box(fltk::NO_BOX);
-	textdisplay4->copy_label("hello word");  
-	textdisplay4->align(fltk::ALIGN_LEFT|fltk::ALIGN_INSIDE);
-	
+	textset(textdisplay1,64,"hello world!!!");
+	textset(textdisplay2,64,"hello world!!!");
+	textset(textdisplay3,64,"hello world!!!");
+	textset(textdisplay4,64,"hello world!!!");
 	group1->show();
 	win->redraw();
 }
@@ -131,46 +114,16 @@ void* prime_func(void* p)
 	}
 	
 	box->hide();
-	textdisplay1->labelsize(32); 
-	textdisplay1->box(fltk::NO_BOX);
-	textdisplay1->copy_label("hello word1");  
-	textdisplay1->align(fltk::ALIGN_LEFT|fltk::ALIGN_INSIDE);
-	
-	textdisplay2->labelsize(32); 
-	textdisplay2->box(fltk::NO_BOX);
-	textdisplay2->copy_label("hello word2");  
-	textdisplay2->align(fltk::ALIGN_LEFT|fltk::ALIGN_INSIDE);
-	
-	textdisplay3->labelsize(32); 
-	textdisplay3->box(fltk::NO_BOX);
-	textdisplay3->copy_label("hello word3");  
-	textdisplay3->align(fltk::ALIGN_LEFT|fltk::ALIGN_INSIDE);
-	
-	textdisplay4->labelsize(32); 
-	textdisplay4->box(fltk::NO_BOX);
-	textdisplay4->copy_label("hello word4");  
-	textdisplay4->align(fltk::ALIGN_LEFT|fltk::ALIGN_INSIDE);
-	
-	textdisplay5->labelsize(32); 
-	textdisplay5->box(fltk::NO_BOX);
-	textdisplay5->copy_label("weintek lab1");  
-	textdisplay5->align(fltk::ALIGN_LEFT|fltk::ALIGN_INSIDE);
-	
-	textdisplay6->labelsize(32); 
-	textdisplay6->box(fltk::NO_BOX);
-	textdisplay6->copy_label("weintek lab2");  
-	textdisplay6->align(fltk::ALIGN_LEFT|fltk::ALIGN_INSIDE);
-	
-	textdisplay7->labelsize(32); 
-	textdisplay7->box(fltk::NO_BOX);
-	textdisplay7->copy_label("weintek lab3");  
-	textdisplay7->align(fltk::ALIGN_LEFT|fltk::ALIGN_INSIDE);
-	
-	textdisplay8->labelsize(32); 
-	textdisplay8->box(fltk::NO_BOX);
-	textdisplay8->copy_label("weintek lab4");  
-	textdisplay8->align(fltk::ALIGN_LEFT|fltk::ALIGN_INSIDE);
+	textset(textdisplay1,64,"hello world1 $$$$$$$$");
+	textset(textdisplay2,64,"hello world1 $$$$$$$$");
+	textset(textdisplay3,64,"hello world1 $$$$$$$$");
+	textset(textdisplay4,64,"hello world1 $$$$$$$$");
 
+	textset(textdisplay5,64,"weintek lab @@@@@@");
+	textset(textdisplay6,64,"weintek lab @@@@@@");
+	textset(textdisplay7,64,"weintek lab @@@@@@");
+	textset(textdisplay8,64,"weintek lab @@@@@@");
+	
 	win->redraw();
 	for (count=0;count<100;count++){
 		usleep(5000);
@@ -194,63 +147,43 @@ void* prime_func(void* p)
 
 int main (int argc, char **argv) 
 {
-	//fltk::Window* win= new fltk::Window(800, 480,"single thread");
 	win= new fltk::Window(800, 480,"single thread");
-	//fltk::Widget* box= new fltk::Widget(10, 10, 780, 460);
-	//Window* win= new Window(800, 480,"single thread");
-	//fltk::Window win(800, 480);
 	win->begin();
 
-	//button = new ToggleButton(55,55,300,300,"Pixmap");
-	//button->image(image); 
-
-	//Widget* box= new Widget(10, 10, 780, 400);
 	box = new Widget(10, 10, 780, 400);
 
-#if 1
 	Button* pageswitch1 = new Button(50,420,80,40,"png");
 	pageswitch1->callback(png_cb);
-	//pageswitch1->when(fltk::WHEN_CHANGED);
 	
 	Button* pageswitch2 = new Button(200,420,80,40,"jpeg");
 	pageswitch2->callback(jpeg_cb);
-	//pageswitch2->when(fltk::WHEN_CHANGED);
 		
 	Button* pageswitch3 = new Button(350,420,80,40,"text");
 	pageswitch3->callback(text_cb);
-#endif	
 
 	image=imagepng1;
-	//win->image(image);
 	
 	box->image(image);
 	box->redraw();  
 		
-#if 1
-	group1 = new PackedGroup(20,20,250,160); 
+	group1 = new PackedGroup(20,20,750,320); 
 	group1->begin(); 
-	textdisplay1 = new Widget(20,20,250,40);
-	textdisplay2 = new Widget(20,20,250,40);
-	textdisplay3 = new Widget(20,20,250,40);
-	textdisplay4 = new Widget(20,20,250,40);
+	textdisplay1 = new Widget(20,20,750,80);
+	textdisplay2 = new Widget(20,20,750,80);
+	textdisplay3 = new Widget(20,20,750,80);
+	textdisplay4 = new Widget(20,20,750,80);
 	group1->end(); 
 	
-	group2 = new PackedGroup(20,20,250,160); 
+	group2 = new PackedGroup(20,20,750,320); 
 	group2->begin(); 
-	textdisplay5 = new Widget(200,20,250,40);
-	textdisplay6 = new Widget(200,20,250,40);
-	textdisplay7 = new Widget(200,20,250,40);
-	textdisplay8 = new Widget(200,20,250,40);
+	textdisplay5 = new Widget(20,20,750,80);
+	textdisplay6 = new Widget(20,20,750,80);
+	textdisplay7 = new Widget(20,20,750,80);
+	textdisplay8 = new Widget(20,20,750,80);
 	group2->end(); 
 
-	//fltk::create_thread(prime_thread, show_message, box);
-#endif	
-
-	//win->resizable(win);
 	win->end();
 	win->show(argc,argv);
-	//create_thread(prime_thread, show_message, box);
-	//fltk::run();
 	fltk::lock();
 	fltk::create_thread(prime_thread, prime_func, box); 
 	return fltk::run();
